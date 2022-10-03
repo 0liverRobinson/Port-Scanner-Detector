de6341580e0219e04b69cdec7839b803b95dde76
Oliver <50546763+0liverRobinson@users.noreply.github.com>
2022-10-04 00:04:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -52,14 +52,14 @@ def detect_port_scan():
             continue
         # Remove packet lists from capture
         remove_list = []
-        for _, packet_cap in packet_dictionary.items():
+        for _, packet_cap in packet_dictionary_buffer.items():
             # Check to see if its over the threshhold
                 if (packet_cap.tailLength >= PORT_COUNT_THRESHOLD):
                     # Alert the user of suspicious activity
-                    if (packet_cap.source_ip not in unsafe_packet_dictionary):
+                    if (packet_cap.source_ip not in attackers_packets_dictionary):
                         print("[", packet_cap.source_ip,"\033[91m\033[1mDETECTED PORT SCANNING\033[0m ]")
                         # Append to the unsafe packet dictionary
-                        unsafe_packet_dictionary[packet_cap.source_ip] = packet_cap
+                        attackers_packets_dictionary[packet_cap.source_ip] = packet_cap
                     # Open the log file to do with the IP address
                     log_file = open( "logs/" + packet_cap.source_ip.replace(".", "_") + ".log", "a")
                     # Write to the file
@@ -70,7 +70,7 @@ def detect_port_scan():
         dictionary_lock.release()
         # Remove printed packets from packet dictionary
         for ip_address in remove_list:
-            del packet_dictionary[ip_address]
+            del packet_dictionary_buffer[ip_address]
 
 
 def isNewPort(packet_cap: PacketCap, selected_port: string) -> boolean:
@@ -110,18 +110,18 @@ def deconstruct_packet(packet_captured):
             while not dictionary_lock.acquire():
                 continue
                 
-            if ( source_ip not in packet_dictionary ):
+            if ( source_ip not in packet_dictionary_buffer ):
             # Create a new key in the dictionary
-                packet_dictionary[source_ip] = compiled_packet
+                packet_dictionary_buffer[source_ip] = compiled_packet
                 dictionary_lock.release()
                  
             else:
             # Add to the start of a dictionary linked list
-                compiled_packet.tailLength = 1 + packet_dictionary[source_ip].tailLength    # Increase head's leangth by one from the last 
-                compiled_packet.nextNode = packet_dictionary[source_ip]                     # Set head next to the rest of the list
-                if (isNewPort(packet_dictionary[source_ip], source_port)):                  # Determine weatehr we are scanning a new port or not
-                    packet_dictionary[source_ip] = compiled_packet                          # If we are scanning a new port, add it to the linked list
-                del unsafe_packet_dictionary[source_ip]
+                compiled_packet.tailLength = 1 + packet_dictionary_buffer[source_ip].tailLength    # Increase head's leangth by one from the last 
+                compiled_packet.nextNode = packet_dictionary_buffer[source_ip]                     # Set head next to the rest of the list
+                if (isNewPort(packet_dictionary_buffer[source_ip], source_port)):                  # Determine weatehr we are scanning a new port or not
+                    packet_dictionary_buffer[source_ip] = compiled_packet                          # If we are scanning a new port, add it to the linked list
+                del attackers_packets_dictionary[source_ip]
                 dictionary_lock.release()                
 
     except:
@@ -148,9 +148,9 @@ if __name__ == "__main__":
     # Lock for safe editing of the IP-PORT dictionary
     dictionary_lock = threading.Lock()
     
-    # Dictionaries for packets and unsafe packets
-    packet_dictionary = { }
-    unsafe_packet_dictionary = { }
+    # Dictionaries for current packets and unsafe packets
+    packet_dictionary_buffer = { }
+    attackers_packets_dictionary = { }
 
     # Start program
     Thread(target=fetch_thread).start()                                                     # Fetch Data
